5d27c7ad4a03966ac7198f820d5a1e80a7028f9b
jose.iglesias <jose.iglesias@106-14.info2>
2024-09-20 14:18:11 +0200

    ejercicio terminado

---
+++ Exercise5/src/Exercise5.java
@@ -0,0 +1,11 @@
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		System.out.println("José Iglesias Rodríguez");
+		System.out.println("Calle Ducado 15");
+		System.out.println("641217876");
+
+	}
+
+}
